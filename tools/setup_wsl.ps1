b2fa0d3063daf058ac62b64be9070787611f3175
San <sganis@gmail.com>
2022-12-07 09:25:44 +0300

    Added setup wsl ubuntu 20.04

---
+++ tools/setup_wsl.ps1
@@ -0,0 +1,71 @@
+# Rustyssh
+# 07/11/2022, sganis
+# Install WSL with ubuntu2004, ssh server and support user
+# For testing purposes
+# It works for new development machines and also in Appveyor
+#
+# Run scripts in powershell:
+# set-executionpolicy remotesigned
+#
+# Uninstall:
+# wslconfig /u Ubuntu-18.04
+
+Write-host "Checking if WSL feature is installed..."
+$installed = (Get-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online).State -eq 'Enabled'
+if ($installed) {
+    Write-host "WSL feature is installed"
+} else {   
+    Write-error "WSL feature is not installed, installing..."
+    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+}
+
+# $zip = "C:\cache\ubuntu1804.zip"
+# $exe = "C:\MyWSL\ubuntu1804\ubuntu1804.exe"
+# $url = "https://aka.ms/wsl-ubuntu-1804"
+# $folder = "C:\MyWSL\ubuntu1804"
+
+$zip = "C:\cache\ubuntu2004.zip"
+$exe = "C:\MyWSL\ubuntu2004\ubuntu2004.exe"
+$url = "https://wsldownload.azureedge.net/Ubuntu_2004.2020.424.0_x64.appx"
+# $url = "# https://aka.ms/wslubuntu2004" # this has many versions
+$folder = "C:\MyWSL\ubuntu2004"
+
+
+New-Item -ItemType Directory -Force -Path C:\MyWSL
+
+if (!(Test-Path $exe)) {
+    Write-host "Installing Ubuntu for WSL"
+    if (!(Test-Path $zip)) {
+        Write-host "Downloading..."
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        (New-Object Net.WebClient).DownloadFile("$url", "$zip")
+    } else {
+        Write-host "Downloaded already, found in cache..."
+    }
+    Write-host "Installing..."
+    Expand-Archive -Path "$zip" -DestinationPath "$folder" -Force
+    . $exe install --root
+}
+
+. $exe run sudo adduser support --gecos `"First,Last,RoomNumber,WorkPhone,HomePhone`" --disabled-password
+. $exe run sudo "echo 'support:support' | sudo chpasswd"
+. $exe run sudo usermod -aG sudo support
+. $exe run sudo "echo -e `"`"support\tALL=(ALL)\tNOPASSWD: ALL`"`" > /etc/sudoers.d/support 2>/dev/null"
+. $exe run chmod 0755 /etc/sudoers.d/support
+
+
+# Write-host "Updating..."
+# . $exe run sudo apt-get update
+# Write-host "Checing user..."
+# . $exe run whoami
+
+Write-host "Installing ssh..."
+. $exe run sudo apt-get update
+. $exe run sudo apt-get remove -y -qq --purge openssh-server `>`/dev/null
+. $exe run sudo apt-get install -y -qq openssh-server `>`/dev/null
+. $exe run sudo service ssh --full-restart
+
+# Write-Host "Export only available from Version 10.0.20363.720"
+# $host
+# Write-host "Exporting..."
+# wsl --export Ubuntu-20.04 $tar
