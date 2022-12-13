b5bd47a4ab2648cc6bc064d46f067ce4d6a071d1
San <sganis@gmail.com>
2022-12-13 08:17:03 +0300

    Added missing file

---
+++ src/lib/FilePage.svelte
@@ -0,0 +1,17 @@
+<script>
+import {PageStore} from '../js/store'
+</script>
+
+<div class="scrollable">
+    {@html $PageStore.replaceAll("\n","<br/>")}
+</div>
+
+<style>
+    .scrollable {
+      overflow-y: auto; 
+      height: 100vh; 
+      scrollbar-gutter: stable;
+      margin: 15px;
+      padding: 10px; 
+    }
+</style>
